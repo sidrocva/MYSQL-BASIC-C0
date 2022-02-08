ce3ea1473c93e7ba5f1fa550ffd1eeada2551fe5
sidrocva <2055851@talnet.nl>
2022-02-08 10:44:31 +0100

    opdracht 1 klaar

--- 01-Databases/Opdracht01-database-import/antwoorden.sql
+++ 01-Databases/Opdracht01-database-import/antwoorden.sql
@@ -1,1 +1,7 @@
 -- Taak01 - haal de tabel movies op:
+SELECT * FROM jaar2016  
+SELECT * FROM jaar2015
+SELECT country, score FROM jaar2016
+SELECT region FROM jaar2015
+SELECT region, score FROM jaar2015
+SELECT score, rank FROM jaar2016
