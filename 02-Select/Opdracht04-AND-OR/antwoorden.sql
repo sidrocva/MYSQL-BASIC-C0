1a13c45494dd83baacd56ac0ccbad10edd3a9347
sidrocva <2055851@talnet.nl>
2022-03-03 19:42:30 +0100

    opdracht4 klaar

--- 02-Select/Opdracht04-AND-OR/antwoorden.sql
+++ 02-Select/Opdracht04-AND-OR/antwoorden.sql
@@ -1,30 +1,30 @@
 -- Opdracht 1 
-
+SELECT * WHERE nationality="Spain" AND club="Chelsea"
 -- Opdracht 2 
-
+SELECT * WHERE age=17 and nationality="spain" and wage=15000
 -- Opdracht 3
-
+select * where club="Liverpool" AND age>20
 -- Opdracht 4
-
+select * where country="Netherlands" and club="ajax"
 -- Opdracht 5
-
+select * where club="ajax" and nationality=!"netherlands"
 -- Opdracht 6 
-
+select name, age where club="AZ alkmaar"
 -- Opdracht 7 
-
+select name, age where club="AZ alkmaar"
 -- Opdracht 8
-
+select name, wage where nationality="Brazilian" and club="Manchester City"
 -- Opdracht 9
-
+select name where age=30 and wage<10000
 -- Opdracht 10
-
+select name, age where nationality="Porugal" or spain
 -- Opdracht 11
-
+select name, age where nationality="Portugal" and club="Chelsea"
 -- Opdracht 12
-
+select name, club where age>40 and wage=10000
 -- Opdracht 13
-
+select * where country="Netherlands" and club="Ajax" or club="FC Utrecht"
 -- Opdracht 14
-
+select * where country="England" and age>20 and wage>100000
 -- Opdracht 15
-
+select name, age, nationality where nationality="Argentinië" or nationality="Brazilië" and age>25
