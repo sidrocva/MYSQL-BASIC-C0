b732d102ac4b2d8c9ad0e4e320f1b4a4333de750
sidrocva <2055851@talnet.nl>
2022-03-08 14:05:11 +0100

    opdracht 6 klaar

--- 03-Delete/Opdracht06-DELETE/antwoorden.sql
+++ 03-Delete/Opdracht06-DELETE/antwoorden.sql
@@ -1,9 +1,10 @@
 -- Opdracht 1 
-
+delete from players where name = "david silva";
 -- Opdracht 2 
-
+delete from players where club="willem II";
 -- Opdracht 3
-
+delete from players where nationality="belgium" and club="fc barcelona";
 -- Opdracht 4
-
+delete from players where age=>38;
 -- Opdracht 5
+delete from players where id=167905 or 169595;
